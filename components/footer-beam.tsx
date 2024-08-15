0de44058513bb8c266b42d4e02705205611e2d64
ReedLake <reedlake@outlook.com>
2024-08-15 17:25:46 +0800

    edit the footer

--- components/footer-beam.tsx
+++ components/footer-beam.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function FooterItem() {
     return (
@@ -11,8 +12,8 @@ export default function FooterItem() {
                     <div className="text-xl text-zinc-500">Free VR Porn</div>
                 </div>
             </div>
-            <p className="text-base text-center text-zinc-500 mx-auto max-w-2xl">The website focusing on VR collections, and continuously in upgrading, please stay tuned...</p>
-
+            <p className="text-base text-center text-zinc-500 mx-auto max-w-2xl">🎦 Free-vrporn.com collects all the best websites for Free VR Porn videos, VR Porn Games, VR Sex Games, Hentai VR, VR Sex Tubes, and
+                VR adult entertainment in all forms for various sexual orientations.</p>
             <div className="text-sm text-zinc-500 text-center">© 2024 Free-VRPorn.com. All Rights Reserved.</div>
         </div>
     );
